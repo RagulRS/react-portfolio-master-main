13e225aedff218febeec82a4f12d393b8f7cc92b
RagulRS <ragulsingh6245@gmail.com>
2024-11-11 00:39:11 +0530

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,133 @@
+const logotext = "Ragul Singh D";
+const meta = {
+    title: "Ragul Singh D",
+    description: "I’m Ragul Singh Full stack devloper,currently working in Coimbatore",
+};
+
+const introdata = {
+    title: "I’m Ragul Singh",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "I love to create posters, cards designs etc.. ",
+    },
+    description: "I am a Full Stack MERN developer with expertise in MongoDB, Express.js, React.js, and Node.js. Skilled in both frontend (HTML, CSS, JavaScript, React) and backend (Node.js, Express, MongoDB) development, I build responsive, scalable web applications with seamless user experiences. My knowledge in Azure deployment further enhances my ability to deliver high-performance applications. With a strong foundation in full-stack development and a user-focused approach, I am prepared to make a valuable impact on dynamic, forward-thinking teams.",
+    your_img_url: "https://i.imgur.com/uoCywHa.jpeg",
+};
+
+const dataabout = {
+    title: "About Myself",
+    aboutme: "I am a Full Stack MERN developer with expertise in MongoDB, Express.js, React.js, and Node.js. Skilled in both frontend (HTML, CSS, JavaScript, React) and backend (Node.js, Express, MongoDB) development, I build responsive, scalable web applications with seamless user experiences. My knowledge in Azure deployment further enhances my ability to deliver high-performance applications. With a strong foundation in full-stack development and a user-focused approach, I am prepared to make a valuable impact on dynamic, forward-thinking teams.",
+};
+const worktimeline = [{
+        jobtitle: "Full-Stack Developer",
+        where: "Guvi",
+        date: "2024",
+    },
+    {
+        jobtitle: "Programmer Analyst",
+        where: "Cognizant",
+        date: "2023-2024",
+    },
+    {
+        jobtitle: "Programmer Analyst Trainee",
+        where: "Cognizant",
+        date: "2022",
+    },
+];
+
+const skills = [{
+        name: "HTML",
+        value: 90,
+    },
+    {
+        name: "CSS",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 80,
+    },
+    {
+        name: "Mongo DB",
+        value: 85,
+    },
+    {
+        name: "Java",
+        value: 50,
+    },{
+        name: "Adobe Photoshop",
+        value: 85,
+    },{
+        name: "Adobe Premire Pro",
+        value: 70,
+    },
+];
+
+const services = [
+];
+
+const dataportfolio = [{
+        img: "https://i.imgur.com/t9cXw5O.jpeg",
+        description: "The Vehicle Care Platform",
+        link: "https://rsvehiclecareplatform.netlify.app/",
+    },
+    {
+        img: "https://i.imgur.com/PgEcUj2.jpeg",
+        description: "Pokemon Card Generator",
+        link: "https://pokemonappbyrs.netlify.app/",
+    },
+    {
+        img: "https://i.imgur.com/Z82A6j9.jpeg",
+        description: "Demo Mobile phones E-Commerce website",
+        link: "https://phonescartpage.netlify.app/",
+    },
+    {
+        img: "https://i.imgur.com/CB02s2T.jpeg",
+        description: "Basic Calculator",
+        link: "https://rscalculator.netlify.app/",
+    },
+    {
+        img: "https://i.imgur.com/Pd1ATle.jpeg",
+        description: "Weather App",
+        link: "https://weatherappbyrs.netlify.app/",
+    },
+    {
+        img: "https://i.imgur.com/lZNBKMU.jpeg",
+        description: "Image-Search App",
+        link: "https://imagesearchappbyrs.netlify.app/",
+    },
+    
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "ragulsingh6245@gmail.com",
+    YOUR_FONE: "+91 6383251292",
+    description: "I am a Full Stack MERN developer with a strong foundation in creating responsive and user-centric websites. I enjoy bringing both technical and creative elements into my work—my passion for content creation complements my development skills, as I’m experienced in video editing and Photoshop. I also have a background in clinical data, which helps me approach healthcare-related tech projects with valuable insight. Additionally, I have knowledge of Temenos Transact banking systems, equipping me to contribute to fintech projects and digital banking solutions. ",
+    YOUR_SERVICE_ID: "service_25njo9b",
+    YOUR_TEMPLATE_ID: "template_1iykxav",
+    YOUR_USER_ID: "Y9F6BMLOUZy8kaJmI",
+};
+
+const socialprofils = {
+    github: "https://github.com/RagulRS",
+    facebook: "https://facebook.com/ragul.reigns.351/",
+    linkedin: "https://www.linkedin.com/in/ragul-singh-d-6a2b4c5/",
+    instagram: "https://www.instagram.com/_ragul_rs_05/",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
